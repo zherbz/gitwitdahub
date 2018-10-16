688fc5af790aebf0ca2e302fa006099ea0162fc5
Zach <zherbz32@gmail.com>
2018-10-16 14:45:26 -0600

    Added line of code
    Added line of code to the overall script

---
+++ NewSRC/SampleFunction.R
@@ -0,0 +1,10 @@
+#test script for GitHub
+myfunction <- function(x,y){
+  if(x>y){
+    print("x is greater than y")
+  }else{
+    print("y is greater than or equal to x")
+  }
+}
+#This is my addition to the code
+z<-myfunction(x=10,y=5)
